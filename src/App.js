1206614b22e8b46529df0c822daad043eefb57a8
Pierre Basck <pierre.basck@gmail.com>
2022-10-12 17:23:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Component1 from "./components/component1";
+
+function App() {
+  return (
+    <div className="classFruit">
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/component1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Component1(){
+
+  return (
+    <test>Hi</test>
+  );
+}
+
+export default Component1;
